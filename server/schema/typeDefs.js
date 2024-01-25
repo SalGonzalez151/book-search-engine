ea095783374702abf6842f9524bbb02c30b931b1
SalGonzalez151 <imdone151@gmail.com>
2024-01-25 13:16:49 -0800

    added files and updated the server file

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,15 @@
+const typeDefs = `
+type Book {
+    _id: ID!
+    authors: String
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+}
+
+type User {
+    
+}
+`
